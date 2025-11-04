d256c69dbaa79f7cdbec7fb4b018478f0c762b6c
HenriqueDML <henrique.garcia.cap698@gmail.com>
2025-11-04 15:57:08 -0300

    Atualizando botões

--- API/VisionHive.Domain/obj/Debug/net8.0/VisionHive.Domain.AssemblyInfo.cs
+++ API/VisionHive.Domain/obj/Debug/net8.0/VisionHive.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VisionHive.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69c9781f3e2a79c9a700f4c3098afc6a6e0b1a86")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1169478503762ad6db9e4eaf221e64ead489a97")]
 [assembly: System.Reflection.AssemblyProductAttribute("VisionHive.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VisionHive.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
